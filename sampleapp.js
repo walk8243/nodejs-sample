5a01e8cbd2f3890923884ab7938e8d9f125c175a
walk8243 <walk8243@gmail.com>
2017-12-13 03:09:57 +0900

    ルーティングの実装
    favicon.icoが無いせいで、何かエラー吐いてるけど、無視無視(笑)

--- sampleapp.js
+++ sampleapp.js
@@ -1,9 +1,29 @@
 var http	= require('http'),
 		fs		= require('fs'),
-		ejs		= require('ejs');
+		ejs		= require('ejs'),
+		url		= require('url');
 
-var hello = fs.readFileSync('./hello.ejs', 'utf8');
-var content1 = fs.readFileSync('./content1.ejs', 'utf8');
+var template = fs.readFileSync('./template.ejs', 'utf8');
+var content2 = fs.readFileSync('./content2.ejs', 'utf8');
+var content3 = fs.readFileSync('./content3.ejs', 'utf8');
+
+var routes = {
+	"/": {
+		"title": "Main Page",
+		"message": "これはサンプルのページです。",
+		"content": content2
+	},
+	"/index": {
+		"title": "Main Page",
+		"message": "これはサンプルのページです。",
+		"content": content2
+	},
+	"/other": {
+		"title": "Other Page",
+		"message": "別のページです。",
+		"content": content3
+	}
+};
 
 var server = http.createServer();
 server.on('request', doRequest);
@@ -12,17 +32,30 @@ console.log('Server running!');
 
 // リクエストの処理
 function doRequest(req, res){
-	var hello2 = ejs.render(hello, {
-		title: "タイトルです",
-		content: ejs.render(content1, {
-			data: [
-				"これは最初のデータです。",
-				"次のデータだよ。",
-				"一番最後のデータなのだ。"
-			]
-		})
+	var url_parts = url.parse(req.url);
+	// console.log(url_parts);
+	// return;
+
+	// route check
+	if(routes[url_parts.pathname] == null){
+		console.log("NOT FOUND PAGE: " + req.url);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write("<html><body><h1>NOT FOUND PAGE:" + req.url + "</h1></body></html>");
+		res.end();
+		return;
+	}
+
+	// page render
+	var content = ejs.render(template, {
+		title: routes[url_parts.pathname].title,
+		content: ejs.render(
+			routes[url_parts.pathname].content,
+			{
+				message: routes[url_parts.pathname].message
+			}
+		)
 	});
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(hello2);
+	res.write(content);
 	res.end();
 }
