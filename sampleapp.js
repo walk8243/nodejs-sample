7afa7be5b9ea7ce5cc025bc3569ae2bdf84a6f60
walk8243 <walk8243@gmail.com>
2017-12-13 02:58:49 +0900

    外部ファイルから表示

--- sampleapp.js
+++ sampleapp.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request', doRequest);
@@ -7,7 +8,13 @@ console.log('Server running!');
 
 // リクエストの処理
 function doRequest(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello World\n');
-	res.end();
+	fs.readFile(
+		'./hello.html',
+		'UTF-8',
+		function(err, data){
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+		}
+	);
 }
